e8ee733bb005155a557951feb0dd7cb951664b4f
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2024-03-24 23:15:38 +0200

    3rd task done.

---
+++ lab-2/task3_singleton/Authenticator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_singleton
+{
+    public class Authenticator
+    {
+        private static Authenticator instance;
+        private static readonly object lockObject = new object();
+
+        private Authenticator() { }
+
+        public static Authenticator GetInstance()
+        {
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = new Authenticator();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ lab-2/task3_singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using task3_singleton;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Authenticator authenticator1 = Authenticator.GetInstance();
+        Authenticator authenticator2 = Authenticator.GetInstance();
+
+        Console.WriteLine($"Are authenticator1 and authenticator2 the same instance? {(authenticator1 == authenticator2 ? "Yes" : "No")}");
+    }
+}
