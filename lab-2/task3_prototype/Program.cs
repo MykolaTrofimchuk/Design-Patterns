3ed7f92234d3ef8f18ac84fee53ec8ee603980f5
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2024-03-24 23:05:25 +0200

    4th task done.

---
+++ lab-2/task3_prototype/Program.cs
@@ -0,0 +1,28 @@
+﻿using task3_prototype;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Virus firstGenerationVirus = new Virus(1.5, 1, "First Gen Virus", "A");
+        Virus secondGenerationVirus1 = new Virus(1.2, 1, "Second Gen Virus 1", "B");
+        Virus secondGenerationVirus2 = new Virus(1.4, 1, "Second Gen Virus 2", "B");
+        Virus thirdGenerationVirus1 = new Virus(1.1, 1, "Third Gen Virus 1", "C");
+        Virus thirdGenerationVirus2 = new Virus(1.3, 1, "Third Gen Virus 2", "C");
+
+        firstGenerationVirus.AddChild(secondGenerationVirus1);
+        firstGenerationVirus.AddChild(secondGenerationVirus2);
+        secondGenerationVirus1.AddChild(thirdGenerationVirus1);
+        secondGenerationVirus1.AddChild(thirdGenerationVirus2);
+
+        Virus clonedVirus = (Virus)firstGenerationVirus.Clone();
+
+        Console.WriteLine("Cloned Virus Information:");
+        clonedVirus.DisplayInfo();
+
+        Virus clonedVirus2 = (Virus)secondGenerationVirus1.Clone();
+
+        Console.WriteLine("\nCloned Virus Information №2:");
+        clonedVirus2.DisplayInfo();
+    }
+}
---
+++ lab-2/task3_prototype/Virus.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_prototype
+{
+    // Клонування відбувається за допомогою інтерфейсу ICloneable, який дозволяє створювати глибокі копії об'єктів. У класі Virus реалізовано метод Clone(),
+    // який створює новий об'єкт Virus з такими ж властивостями, як у вихідному вірусі. Після цього рекурсивно клонуються всі діти вихідного вірусу і
+    // додаються до клонованого вірусу.
+    public class Virus : ICloneable
+    {
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public List<Virus> Children { get; set; }
+
+        public Virus(double weight, int age, string name, string kind)
+        {
+            Weight = weight;
+            Age = age;
+            Name = name;
+            Kind = kind;
+            Children = new List<Virus>();
+        }
+
+        public void AddChild(Virus child)
+        {
+            Children.Add(child);
+        }
+
+        public object Clone()
+        {
+            Virus clonedVirus = new Virus(Weight, Age, Name, Kind);
+
+            foreach (var child in Children)
+            {
+                clonedVirus.AddChild((Virus)child.Clone());
+            }
+
+            return clonedVirus;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Virus Kind: {Kind}, Weight: {Weight}, Age: {Age}");
+            Console.WriteLine("Children:");
+            foreach (var child in Children)
+            {
+                Console.WriteLine($" - {child.Name}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
